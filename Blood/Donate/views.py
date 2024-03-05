8cab073d3965a87ceb690cf5cf2ce15429c34578
vigneshtpm <vigneshwarantpm2@gmail.com>
2024-03-05 17:46:13 +0530

    first commit

---
+++ Blood/Donate/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request,"Donate/index.html",{})
+
+def register(request):
+    return render(request,"Donate/register.html",{})
+
+def login(request):
+    return render(request,"Donate/login.html",{})
+
+
+
